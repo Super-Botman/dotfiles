ce8bff3cd6819103b87a65e4dab57ac3e97c1d0f
Botman <velayismaa@gmail.com>
2023-08-28 22:59:42 +0200

    correct some errors in the install script

--- install.sh
+++ install.sh
@@ -4,34 +4,33 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo "Install all tools"
-pacman -S alacritty awesome dunst neofetch i3 polybar rofi picom skeuos-gtk-theme-git lxaperance sddm cargo
+pacman -S alacritty awesome dunst neofetch i3 polybar rofi picom skeuos-gtk-theme-git lxappearance sddm cargo
 
 echo "backup .config into .config.bak"
-cp -r -p $HOME/.config $HOME/.config.bak
+if ! [ -d $HOME/.config ]; then
+   cp -r -p $HOME/.config $HOME/.config.bak
+fi
 
 echo "copying wallpaper"
 if [ -d /usr/share/backgrounds ]; then
-  mv wallpaper-cbp.jpg /usr/share/backgrounds
+  mv wallpaper/wallpaper-cbp.jpg /usr/share/backgrounds
 else
   mkdir /usr/share/backgrounds
-  mv wallpaper-cbp.jpg /usr/share/backgrounds
+  mv wallpaper/wallpaper-cbp.jpg /usr/share/backgrounds
 fi
 
 echo "install config"
 mv .config $HOME/.config
 
-echo "restarting i3"
-i3 restart
-
 echo 'select the theme "skeuos-dark" and icons "papirus"'
 lxappearance
 
-echo "setup sddm"
-mv sddm/sddm.conf
-
 echo "install sddm theme"
 git clone https://framagit.org/MarianArlt/sddm-sugar-candy.git /usr/share/sddm/themes/
-mv sddm/theme.conf /usr/share/sddm/themes/sugar-candy
+
+echo "setup sddm"
+cp sddm/sddm.conf /etc
+cp sddm/theme.conf /usr/share/sddm/themes/sugar-candy
 
 echo "nerd font install"
 tar -xvzf fonts/AgaveNerdFont.tar.gz -C $HOME/.local/share/fonts/ttf/AgaveNerdFont
