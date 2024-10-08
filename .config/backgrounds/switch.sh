778f97e4ee6d5a175b3fb421474f014caaa76be8
Botman <velayismaa@gmail.com>
2024-10-08 21:11:43 +0200

    fix wallpaper script

--- .config/backgrounds/switch.sh
+++ .config/backgrounds/switch.sh
@@ -5,19 +5,18 @@ show_wallpaper()
   files=($(ls -d $HOME/.config/backgrounds/$1/*.mp4))
 
   while true; do
-    $HOME/.config/backgrounds/wallpaper.sh -a ${files[i]}
-    sleep $((60*45 + $RANDOM % 60*60))
-    killall -9 mpv
-    if [ ${#files[@]} == $((i+1)) ]; then
+    killall -9 xwinwrap
+    xwinwrap -ov -ni -g 1920x1080+0+0 -- mpv --fs --loop-file --no-audio --no-osc --no-osd-bar -wid WID --no-input-default-bindings ${files[i]} &
+    sleep $((60*20 + $RANDOM % 60*30))
+    i=$((i+1))
+    if [ ${#files[@]} == $i ]; then
       i=0
     fi
-    i=$((i+1))
   done
 }
 
 get_sunset()
 {
- currenttime=$(date +%R)
  request=$(curl -s 'https://api.sunrise-sunset.org/json?lat=44.538631&lng=6.495930&tzid=Europe/Paris')
  sunrise=$(date -d "$(echo $request | jq -r '.results.sunrise')" +%R)
  sunset=$(date -d "$(echo $request | jq -r '.results.sunset')" +%R)
@@ -29,10 +28,11 @@ main()
   sunrise=''
   sunset=''
 
-  killall -9 mpv
+  killall -9 xwinwrap
   get_sunset
 
   while :; do
+   currenttime=$(date +%R)
    new_time=''
    if [[ "$currenttime" < "$sunrise" ]] || [[ "$currenttime" > "$sunset" ]]; then
      new_time='night'
@@ -41,11 +41,14 @@ main()
    fi
 
    if [[ "$time" != "$new_time" ]]; then
-     show_wallpaper $new_time
+     kill %1
+     show_wallpaper $new_time &
      get_sunset
    fi
    time=$new_time
-  done &
+
+   sleep 2
+  done
 }
 
 main
